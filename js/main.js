5011a8a4716094f120c278fd3f4785e4310c80f7
Ignacio Baclini <ignaciobaclini@hotmail.com>
2022-06-30 18:30:39 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let nombre = prompt ("Como te llamas?")
+
+
+let edad = prompt ("Que edad tenes?")
+
+if(edad < 30)  {
+        alert ("Hola, como andas?")
+} else {
+    alert ("Bienvenido, buenos dias")
+}
